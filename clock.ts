6304342c523de4c2756fd93e75ac5b868ab26e89
Damian Gryski <dgryski@gmail.com>
2017-06-12 21:20:05 +0200

    draw angle line all the way through

--- clock.ts
+++ clock.ts
@@ -70,7 +70,8 @@ function drawHand(ctx, pos, length, width) {
   ctx.lineCap = "round";
   ctx.moveTo(0, 0);
   ctx.rotate(pos);
-  ctx.lineTo(0, -length);
+  ctx.lineTo(0, length);
+  ctx.lineTo(0, -length)
   ctx.stroke();
   ctx.rotate(-pos);
 }
